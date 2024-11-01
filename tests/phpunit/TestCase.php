72bbe09971173f7f036635bec1acaef7f8083f3b
rumur <rumur.dev@gmail.com>
2024-11-01 18:29:06 +0100

    chore: add unit test default files

---
+++ tests/phpunit/TestCase.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Rumur\WordPress\JsonWebToken\Tests;
+
+/**
+ * Base test case class for all test cases in this plugin.
+ *
+ * @package Rumur\WordPress\JsonWebToken\Tests
+ */
+abstract class TestCase extends \WP_UnitTestCase {
+
+}
